e62d3ed51359056b9c04f4304fdd34a111b83e26
uxxxknow <DALVgrin2000@mail.ru>
2024-08-18 21:08:00 +0300

    Готовое

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка!'
+    else:
+        r = first / second
+        return r
---
+++ true_math.py
@@ -0,0 +1,12 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        r = first / second
+        return r
+
+
+
+
+
